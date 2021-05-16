d8f98545623b82b97cd80ad8891b033f620e91aa
Antonio Perez <00antonio.perez00@gmail.com>
2021-05-15 22:32:39 -0500

    Receive messages via Bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
 void setup() {
     Serial.begin(115200);
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+    Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-// write your code here
-    Serial.println("Hola mundo. Yei :)");
-    delay(1100);
+    if (Serial.available()) {
+        SerialBT.write(Serial.read());
+    }
+    if (SerialBT.available()) {
+        Serial.write(SerialBT.read());
+    }
+    delay(20);
 }
